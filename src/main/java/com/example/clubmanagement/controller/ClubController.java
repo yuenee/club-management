c219f9ed96921325c27519d342c18764205386c8
DESKTOP-RKMMNC3\user <rekex17@gmail.com>
2023-06-24 15:47:06 +0900

    一覧画面表示

--- src/main/java/com/example/clubmanagement/controller/ClubController.java
+++ src/main/java/com/example/clubmanagement/controller/ClubController.java
@@ -1,12 +1,14 @@
 package com.example.clubmanagement.controller;
 
-import com.example.clubmanagement.repository.entity.Clubs;
-import com.example.clubmanagement.service.ClubsService;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import com.example.clubmanagement.repository.entity.Clubs;
+import com.example.clubmanagement.service.ClubsService;
 
 @Controller
 @RequestMapping("/club")
@@ -18,8 +20,16 @@ public class ClubController {
     }
 
     @GetMapping("/list")
-    public String getClubList() {
+    public String getClubList(Model model) {
         List<Clubs> clubs = this.clubsService.getClubs();
+        Clubs club = new Clubs();
+        club.setClubName("太郎");
+        clubs.add(club);
+        club.setClubName("二郎");
+        clubs.add(club);
+        club.setClubName("三郎");
+        clubs.add(club);
+        model.addAttribute("clubs",clubs);
 
         return "/club-list";
     }
