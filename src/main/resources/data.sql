c81f1b128f6b5eb25296d1190f96d1e1197cfa98
emoto <h0i8e2n4n@yahoo.co.jp>
2023-06-24 15:28:03 +0900

    初期データとテーブル作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO employee (id, name, age)
-VALUES('1', 'Tom', 30);
+INSERT INTO club_members (club_member_id, user_id, club_id, post)
+VALUES('1','1','1','部長');
+
+INSERT INTO clubs (club_id, club_name, request_date, request_name, request_member, club_concept, club_content)
+VALUES('1','ボルダリング','2022-04-04','高谷匠','5','aaa','iii');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,46 @@
-CREATE TABLE IF NOT EXISTS employee (
-  id VARCHAR(50) PRIMARY KEY,
-  name VARCHAR(50),
-  age INT
+CREATE TABLE IF NOT EXISTS users (
+  user_id INT(5) PRIMARY KEY,
+  user_name VARCHAR(20),
+  user_kana_name VARCHAR(30),
+  role VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS clubs (
+  club_id INT(5) PRIMARY KEY,
+  club_name VARCHAR(10) UNIQUE,
+  request_date VARCHAR(10),
+  request_name VARCHAR(20),
+  request_member INT(5),
+  club_concept VARCHAR(200),
+  club_content VARCHAR(200)
+);
+
+CREATE TABLE IF NOT EXISTS club_members (
+ club_member_id INT(5) PRIMARY KEY,
+ user_id INT(5),
+ club_id INT(5),
+ post VARCHAR(10)
+  
+);
+
+CREATE TABLE IF NOT EXISTS club_reports (
+ report_id INT(5) PRIMARY KEY,
+ club_id INT(5),
+ create_member VARCHAR(20),
+ active_member INT(5),
+ active_date VARCHAR(10),
+ active_name VARCHAR(50),
+ active_goal VARCHAR(200),
+ active_plan VARCHAR(200),
+ active_result VARCHAR(200),
+ improvement VARCHAR(200),
+ message VARCHAR(200),
+ picture VARCHAR(1000)
+);
+
+CREATE TABLE IF NOT EXISTS active_members (
+ active_id INT(5) PRIMARY KEY,
+ report_id INT(5),
+ user_id INT(5),
+ thought VARCHAR(200)
 );
